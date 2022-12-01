15c9c2945da0729852dc60a9a022edc1c083026e
Yaduttam Pareek <yaduttampareek95@gmail.com>
2022-12-01 07:38:39 +0530

    Add files via upload

---
+++ Knights Travail.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#define N 8
+int cnt;
+
+int soveknight(int x, int y, int movei, int sol[N][N],
+				int xMove[], int yMove[]);
+
+
+int notvisited(int x, int y, int sol[N][N])
+{
+	return (x >= 0 && x < N && y >= 0 && y < N
+			&& sol[x][y] == -1);
+}
+
+
+void printSolution(int sol[N][N])
+{
+	int x,y;
+	printf("----------------------------------------------------------------");
+		printf("\n");
+	for (x= 0; x < N; x++) {
+		for (y = 0; y < N; y++)
+			printf("|  %2d  |", sol[x][y]);
+		printf("\n");
+		printf("----------------------------------------------------------------");
+		printf("\n");
+	}
+}
+
+
+int solveKT()
+{
+	int x,y;
+	int sol[N][N];
+
+
+	for (x = 0; x < N; x++)
+		for (y = 0; y < N; y++)
+			sol[x][y] = -1;
+
+
+	int xMove[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+	int yMove[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+	
+	sol[0][0] = 0;
+
+
+	if (soveknight(0, 0, 1, sol, xMove, yMove) == 0) {
+		printf("No Solution");
+		return 0;
+	}
+	else
+		printSolution(sol);
+
+	return 1;
+}
+
+
+int soveknight(int x, int y, int movei, int sol[N][N], int xMove[N], int yMove[N])
+{
+	
+	int k, next_x, next_y;
+	if (movei == N * N){
+		return 1;}
+
+
+	for (k = 0; k < 8; k++) {
+		
+		next_x = x + xMove[k];
+		next_y = y + yMove[k];
+		if (notvisited(next_x, next_y, sol)) {
+			sol[next_x][next_y] = movei;
+			cnt++;
+			if (soveknight(next_x, next_y, movei + 1, sol, xMove, yMove)== 1)
+				return 1;
+			else{
+			
+				sol[next_x][next_y] = -1; 
+				}
+		}
+		
+		
+	}
+	
+	return 0;
+}
+
+
+int main()
+{
+	solveKT();
+	printf("cnt   %d", cnt);
+	
+	return 0;
+}
+
